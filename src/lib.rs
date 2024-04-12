9e160c9529793a7f39c5427a2b7c8f579fefcb5c
Sina Najafi <148863702+SinaNajafi1@users.noreply.github.com>
2024-04-12 12:37:58 -0700

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,17 @@
 pub fn collatz(n: u64) -> Option<u64> {
-    unimplemented!(
-        "return Some(x) where x is the number of steps required to reach 1 starting with {n}"
-    )
+    if n < 1 {
+        return None;
+    }
+    let mut inp: u64 = n;
+    let mut count: u64 = 0;
+    while inp != 2 {
+        if inp % 2 == 0 {
+            inp /= 2;
+        }
+        else {
+           inp = inp.checked_mul(3)?.checked_add(1)?;
+        }
+        count = count + 1
+    }
+    Some(count)
 }
